0dd6533a9d8f28f0c01d45a8b5dde86019618365
projeremylemaire-cell <pro.jeremy.lemaire@gmail.com>
2026-04-22 17:21:12 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "eshop-it-request",
+  "version": "1.0.0",
+  "description": "eShop IT Request Form",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
