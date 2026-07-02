d4f99ea4acbd7f8e12a8632b194cc2b0bfd2e2fb
projeremylemaire-cell <pro.jeremy.lemaire@gmail.com>
2026-07-02 17:07:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-
+#
+#
 const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
